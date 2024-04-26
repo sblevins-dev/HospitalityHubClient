bd39efa07cce4b4936ce2f01e58449f73e1a4eca
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-04-26 13:32:23 -0400

    added Mui icons

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -2,6 +2,11 @@ import { styled, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText
 import { NavLink } from "react-router-dom"
 import "./Menu.css"
 import { useTheme } from "@mui/material/styles"
+import SpaceDashboardTwoToneIcon from '@mui/icons-material/SpaceDashboardTwoTone';
+import BedroomChildTwoToneIcon from '@mui/icons-material/BedroomChildTwoTone';
+import PermIdentityTwoToneIcon from '@mui/icons-material/PermIdentityTwoTone';
+import BookOnlineTwoToneIcon from '@mui/icons-material/BookOnlineTwoTone';
+import AdminPanelSettingsTwoToneIcon from '@mui/icons-material/AdminPanelSettingsTwoTone';
 
 const MyNavLink = styled(NavLink)(() => ({
     textDecoration: 'none',
@@ -15,17 +20,23 @@ const MyNavLink = styled(NavLink)(() => ({
 const Menu = () => {
 
     return (
-        <Box bgcolor={'#181d1c'} color={'white'} sx={{ borderRight: '1px solid rgb(240, 240, 240)'}}>
-            <Typography variant="h4" sx={{  paddingY: 4, bgcolor: '#434b50', paddingX: 1, color: 'white', fontFamily: 'fantasy', letterSpacing: '2px' }}>HospitalityHub</Typography>
-            <Box sx={{ marginY: 5, padding: 2,}} elevation={0}>
-                <Typography variant="h5">Michael Levito</Typography>
-                <Typography variant="body2">Administrator</Typography>
+        <Box bgcolor={'#181d1c'} color={'white'} sx={{ borderRight: '1px solid rgb(240, 240, 240)' }}>
+            <Typography variant="h4" sx={{ paddingY: 4, bgcolor: '#434b50', paddingX: 1, color: 'white', fontFamily: 'fantasy', letterSpacing: '2px' }}>HospitalityHub</Typography>
+            <Box sx={{ marginY: 5, padding: 2, }} elevation={0}>
+                <Typography variant="h5" paddingBottom={1}>Michael Levito</Typography>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                    <AdminPanelSettingsTwoToneIcon />
+                    <Typography variant="body2">
+                        Administrator
+                    </Typography>
+                </Box>
             </Box>
-            <Divider />
-            <List >
+            <Divider color='gray' />
+            <List sx={{ marginY: '20px' }}>
                 <ListItem disablePadding>
                     <MyNavLink to="/">
                         <ListItemButton>
+                            <SpaceDashboardTwoToneIcon sx={{ marginRight: '20px' }} />
                             <ListItemText primary="Dashboard" />
                         </ListItemButton>
                     </MyNavLink>
@@ -33,6 +44,7 @@ const Menu = () => {
                 <ListItem disablePadding>
                     <MyNavLink to="/reservations">
                         <ListItemButton>
+                            <BookOnlineTwoToneIcon sx={{ marginRight: '20px' }} />
                             <ListItemText primary="Reservations" />
                         </ListItemButton>
                     </MyNavLink>
@@ -40,6 +52,7 @@ const Menu = () => {
                 <ListItem disablePadding>
                     <MyNavLink to="/guests">
                         <ListItemButton>
+                            <PermIdentityTwoToneIcon sx={{ marginRight: '20px' }} />
                             <ListItemText primary="Guests" />
                         </ListItemButton>
                     </MyNavLink>
@@ -47,12 +60,13 @@ const Menu = () => {
                 <ListItem disablePadding>
                     <MyNavLink to="/rooms">
                         <ListItemButton>
+                            <BedroomChildTwoToneIcon sx={{ marginRight: '20px' }} />
                             <ListItemText primary="Rooms" />
                         </ListItemButton>
                     </MyNavLink>
                 </ListItem>
             </List>
-            <Divider />
+            <Divider color="gray" />
         </Box>
     )
 }
