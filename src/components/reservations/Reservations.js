cb05e0f0967b519fa698dae3c2a0f19b73882ece
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2024-04-26 12:20:23 -0400

    filters list for reservations for reservation page

--- src/components/reservations/Reservations.js
+++ src/components/reservations/Reservations.js
@@ -1,56 +1,44 @@
 import { Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
-import React, { useState } from 'react'
-import api from '../../api/axiosConfig';
-
-
-const rows = [
-    {
-        name: 'Frozen yoghurt',
-        calories: 159,
-        fat: 16.0,
-        carbs: 4,
-        protein: 5
-    },
-    {
-        name: 'Frozen yoghurt',
-        calories: 159,
-        fat: 16.0,
-        carbs: 4,
-        protein: 5
-    }
-];
-
+import React, { useContext, useEffect, useState } from 'react'
+import { MyContext } from '../../context/MyContext';
 
 const Reservations = () => {
 
-    const [reservations, setReservations] = useState([]);
+    const { roomList } = useContext(MyContext);
+    const [reservationList, setReservationList] = useState([]);
+
+    useEffect(() => {
+        let tempList = roomList.filter(room => room.status == 'reserved');
+
+        setReservationList(tempList)
+    }, [])
 
     return (
         <Container>
             <Typography variant="h4" sx={{ marginY: 4 }}>Reservations</Typography>
-            <TableContainer component={Paper} sx={{ minHeight: '700px'}}>
+            <TableContainer component={Paper} sx={{ minHeight: '700px' }}>
                 <Table aria-label='simple table'>
                     <TableHead>
                         <TableRow>
-                            <TableCell>Dessert 100g serving</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat</TableCell>
-                            <TableCell align="right">Carbs</TableCell>
-                            <TableCell align="right">Protein</TableCell>
+                            <TableCell>Room</TableCell>
+                            <TableCell align="right">Beds</TableCell>
+                            <TableCell align="right">Floor Number</TableCell>
+                            <TableCell align="right">Status</TableCell>
+                            <TableCell align="right">Description</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.map(row => (
-                            <TableRow key={row.name}
+                        {roomList.length >= 1 && reservationList.map(room => (
+                            <TableRow key={room.id}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
                                 <TableCell component="th" scope="row">
-                                    {row.name}
+                                    {room.roomNumber}
                                 </TableCell>
-                                <TableCell align="right">{row.calories}</TableCell>
-                                <TableCell align="right">{row.fat}</TableCell>
-                                <TableCell align="right">{row.carbs}</TableCell>
-                                <TableCell align="right">{row.protein}</TableCell>
+                                <TableCell align="right">{room.beds}</TableCell>
+                                <TableCell align="right">{room.floorNumber}</TableCell>
+                                <TableCell align="right">{room.status}</TableCell>
+                                <TableCell align="right">{room.description}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
